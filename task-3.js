6317495d13abb73283893755f8e3f74ed1acd96f
Remi Kujawski <remi.kujawski@gmail.com>
2024-05-19 01:22:07 +0200

    Task 3 completed

---
+++ task-3.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime = hours;
+  },
+  getInfo() {
+    return `${this.username} ma ${this.playTime} aktywnych godzin!`;
+  }
+};
